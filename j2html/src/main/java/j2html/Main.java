c28d98864948fdc32a8fce89840bc7909baaf802
64943127304 <joel.brito-neto@serpro.gov.br>
2021-01-07 16:18:53 -0300

    Adding j2html project

---
+++ j2html/src/main/java/j2html/Main.java
@@ -0,0 +1,13 @@
+package j2html;
+
+import static j2html.TagCreator.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String render = body(
+            h1("Hello, World!"),
+            img().withSrc("/img/hello.png")
+        ).render();
+        System.out.println(render);
+    }
+}
